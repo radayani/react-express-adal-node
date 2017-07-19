82b5f9f94906012bc883c263a8c301bf14eeadfe
raveenadayani <raveenadayani@gmail.com>
2017-07-19 12:40:19 +0530

    chng

--- app.js
+++ app.js
@@ -62,8 +62,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, '/public')));
-app.use(serveStatic(path.join(__dirname,'public',  {'index': ['default.html', 'default.htm']} )))
+app.use('/static', express.static(path.join(__dirname, '/public')));
+// app.use(serveStatic(path.join(__dirname,'public' )))
 app.use(cors());
 
 
