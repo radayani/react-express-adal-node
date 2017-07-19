ac7888449fdf36013bf20155ec6af526b6015dd5
raveenadayani <raveenadayani@gmail.com>
2017-07-19 18:51:22 +0530

    last pull worked! adal, node server, react client all running. lets test apis in this pull

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ app.get('/getAToken', function (req, res) {
     if (err) {
       // window.location.replace(`http://sfvotes.azurewebistes.net/user/${alias}/register`);
       // res.redirect(`/user/${alias}/register`);
-      res.redirect(`/radayani`);
+      res.redirect(`/${alias}`);
       // res.sendFile(__dirname + '/public/index.html');
       return;
     }
@@ -194,14 +194,14 @@ app.get('/getAToken', function (req, res) {
       message += 'refreshResponse: ' + JSON.stringify(refreshResponse);
       // res.redirect(`/user/${alias}/register`);
       // res.sendFile(__dirname + '/public/index.html');
-      res.redirect(`/radayani`);
+      res.redirect(`/${alias}`);
     });
   }
   );
 });
 
 // app.get(`/user/${alias}/register`, function(req, res) {
-app.get(`/radayani`, function(req, res) {
+app.get(`/${radayani}`, function(req, res) {
   res.sendFile(__dirname + '/public/index.html')
 });
 
